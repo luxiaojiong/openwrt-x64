bd7301a2e8a6d13de2cf761851e0cb5ee87c4c1f
luxiaojiong <51263977+luxiaojiong@users.noreply.github.com>
2021-07-05 08:31:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,8 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Add a feed source
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
