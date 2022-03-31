e3fb9349e43ff1957aa6a2579f925d7e89f86484
luxiaojiong <51263977+luxiaojiong@users.noreply.github.com>
2022-03-31 15:39:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # 651 patch
 #cd openwrt/package/kernel/mac80211/patches/rt2x00/
